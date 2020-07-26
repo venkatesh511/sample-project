d760d068d34c01133327d4e75d4387ad7c23d139
venkatesh511 <venkatesh.p511@gmail.com>
2020-07-26 19:10:04 -0400

    conditionals

--- hello.py
+++ hello.py
@@ -1,7 +1,20 @@
 print("hello venkatesh!")
 person22 = {"name":"Jac", "surname":"Smith", "age":"22"}
 #print(person22)///
-print(person22.pop("name"))
-print(person22)
+"""print(person22.pop("name"))
+print(person22)"""
 person22["age"]= 25
 print(person22)
+"""person2= int(input("enter the number:"))
+if person2 in person22.values():
+    print("success")
+else:
+    print("not found")"""
+
+venkat=int(input("enter number:"))
+if venkat>10:
+    print("good")
+elif venkat==10:
+    print("awesome")
+else:
+    print("bad")
